03ac50779fc7beea0f9fc44446171fe53ffeaec6
abdulaziz-mirzaeev <abdulaziz.ismail2003@gmail.com>
2022-06-14 19:30:55 +0500

    db config

--- config/db.php
+++ config/db.php
@@ -1,10 +1,17 @@
 <?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => sprintf('mysql:host=%s;dbname=%s', getenv('DB_HOST'), getenv('DB_NAME')),
-    'username' => getenv('DB_USERNAME'),
-    'password' => getenv('DB_PASSWORD'),
+    'dsn' => sprintf('mysql:host=%s;dbname=%s', $host, $db),
+    'username' => $username,
+    'password' => $password,
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
